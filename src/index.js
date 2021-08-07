ef275494820daee5aabb44fa044675ef82eb3e78
Neha Shanbhag <63840903+nshanbhag20001412@users.noreply.github.com>
2021-08-07 18:29:19 +0530

    updates

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
-
+document.body.style.backgroundImage = "url('./Images/Wallpaper.png')";
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
